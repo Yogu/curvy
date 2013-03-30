f78ca61a2af2c55683ccbead09218ef252fb3cdf
Yogu <info@yogularm.de>
2013-03-30 23:56:05 +0100

    Implementd basic PeerChannel

--- scripts/network/peer-channel.js
+++ scripts/network/peer-channel.js
@@ -1,9 +1,50 @@
 "use strict";
 
 function PeerChannel(connector) {
-	this.connector = connector;
+	var self = this;
+	this._connector = connector;
+	this.state = 'disconnected';
+	this.isConnected = false;
+	this._queue = [];
+	
+	$(connector).on('init', function(e, data) {
+		if (self.state == 'connecting' || self.state == 'disconnected') {
+			if (self.state == 'disconnected')
+				self.connect();
+			self.state = 'connected';
+			self.isConnected = true;
+			self._sendQueue();
+		}
+	});
+	
+	$(connector).on('data', function(e, data) {
+		$(self).triggerHandler(data.event, data.data);
+	});
 }
 
-PeerChannel.prototype.send = function(event, data) {
+PeerChannel.prototype = {
+	connect: function() {
+		if (this.state == 'disconnected') {
+			this.state = 'connecting';
+			this._connector.send('init', null);
+		}
+	},
+		
+	send: function(event, data) {
+		data = typeof(data) == 'undefined' ? null : data;
+		
+		if (this.isConnected) {
+			this._connector.send('data', {event: event, data: data});
+		} else {
+			this._queue.push({event: event, data: data});
+		}
+	},
 	
+	_sendQueue: function() {
+		for (var i = 0; i < this._queue.length; i++) {
+			var item = this._queue[i];
+			this.send(item.event, item.data);
+		}
+		this._queue = [];
+	}
 };
--- spec/peer-channel.js
+++ spec/peer-channel.js
@@ -6,28 +6,50 @@ describe('PeerChannel', function() {
 		connector1 = {
 			send: function(type, data) {
 				$(connector2).triggerHandler(type, data);
+				console.log('1 -> 2: ' + type);
+				if (data) console.log(data);
 			}
 		};
 		connector2 = {
 			send: function(type, data) {
 				$(connector1).triggerHandler(type, data);
+				console.log('2 -> 1: ' + type);
+				if (data) console.log(data);
 			}
 		};
 	});
 	
-	it('should be connecting after creation', function() {
+	it('should be disconnected after creation', function() {
 		var channel = new PeerChannel(connector1);
+		expect(channel.state).toEqual('disconnected');
+		expect(channel.isConnected).toEqual(false);
+	});
+	
+	it('should be connecting after conect()', function() {
+		var channel = new PeerChannel(connector1);
+		channel.connect();
 		expect(channel.state).toEqual('connecting');
 		expect(channel.isConnected).toEqual(false);
 	});
 	
-	it('should connect', function() {
+	it('should accept connection', function() {
 		var channel1 = new PeerChannel(connector1);
 		var channel2 = new PeerChannel(connector2);
-		expect(channel1.state).toEqual('connected');
+		channel1.connect();
+		waitsFor(function() {
+			return channel2.isConnected;
+		}, 'channel2.isConnected should be true');
 		expect(channel2.state).toEqual('connected');
-		expect(channel1.isConnected).toEqual(true);
-		expect(channel2.isConnected).toEqual(false);
+	});
+	
+	it('should respond on connection', function() {
+		var channel1 = new PeerChannel(connector1);
+		var channel2 = new PeerChannel(connector2);
+		channel1.connect();
+		waitsFor(function() {
+			return channel1.isConnected;
+		}, 'channel1.isConnected should be true');
+		expect(channel1.state).toEqual('connected');
 	});
 	
 	it('should send and receive', function() {
@@ -35,6 +57,7 @@ describe('PeerChannel', function() {
 		runs(function() {
 			var channel1 = new PeerChannel(connector1);
 			var channel2 = new PeerChannel(connector2);
+			channel1.connect();
 			$(channel2).on('theevent', function() { isCalled = true; });
 			channel1.send('theevent');
 		});
@@ -49,8 +72,9 @@ describe('PeerChannel', function() {
 		runs(function() {
 			var channel1 = new PeerChannel(connector1);
 			var channel2 = new PeerChannel(connector2);
+			channel1.connect();
 			$(channel2).on('theevent', function(e, data) { isCalled = true; receivedData = data; });
-			channel1.send('theevent');
+			channel1.send('theevent', sentData);
 		});
 		
 		waitsFor(function() { return isCalled;}, "The event listener should be called");
