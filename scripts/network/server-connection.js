59d6e3748b909e59e6d1c2e012b7c5ebc82f0faa
Yogu <info@yogularm.de>
2013-04-03 01:18:32 +0200

    Implemented calling logic

--- scripts/network/server-connection.js
+++ scripts/network/server-connection.js
@@ -5,31 +5,22 @@ function ServerConnection(userName, url) {
 	this.userName = userName;
 	this.state = 'connecting';
 	this.isConnected = false;
+	this.playerState = 'idle';
+	this.peer = null;
 
 	url = (typeof(url) == 'undefined') ? ServerConnection.WEBSOCKET_URL : url;
 	this._socket = io.connect(url, {'force new connection':true});
 
+	// connection
 	this._socket.on('error', function(data) {
 		console.error('socket.io error: ' + data);
 	});
 	this._socket.on('err', function(data) {
 		console.error('server sent error: ' + data.message);
 	});
-	this._socket.on('name_not_available', function(data) {
-		self.state = 'name_not_available';
-		self.isConnected = false;
-	});
-	this._socket.on('accepted', function(data) {
-		self.state = 'connected';
-		self.isConnected = true;
-	});
 	this._socket.on('disconnect', function(data) {
 		this.state = 'disconnected';
 		this.isConnected = false;
-	});
-	this._socket.on('contacts', function(contacts) {
-		self.contacts = contacts.filter(function(c) { return c != self.userName;});
-		$(self).triggerHandler('contacts');
 	});
 	this._socket.on('connect', function() {
 		console.log('socket.io connected');
@@ -38,6 +29,53 @@ function ServerConnection(userName, url) {
 		self.state = 'connect_failed';
 		self.isConnected = false;
 		console.log('socket.io connection failed');
+	});
+	
+	// login
+	this._socket.on('name_not_available', function(data) {
+		self.state = 'name_not_available';
+		self.isConnected = false;
+	});
+	this._socket.on('accepted', function(data) {
+		self.state = 'connected';
+		self.isConnected = true;
+	});
+	
+	// player list
+	this._socket.on('players', function(players) {
+		self.players = players.filter(function(p) { return p.name != self.userName;});
+		$(self).triggerHandler('players');
+	});
+	
+	// calls
+	this._socket.on('call', function(data) {
+		data.accept = function() {
+			self._socket.emit('call', {recipient: data.sender});
+		};
+		data.reject = function() {
+			self._socket.emit('reject', {recipient: data.sender});
+		};
+		$(self).triggerHandler('call', data);
+	});
+	this._socket.on('accept', function(data) {
+		self.playerState = 'busy';
+		self.peer = data.sender;
+		$(self).triggerHandler('accept', data);
+	});
+	this._socket.on('reject', function(data) {
+		// ignore invalid reject messages
+		if (self.playerState == 'calling' && self.peer == data.sender) {
+			self.playerState = 'idle';
+			self.peer = null;
+			$(self).triggerHandler('reject', data);
+		}
+	});
+	this._socket.on('hangup', function(data) {
+		if (self.playerState == 'busy') {
+			self.playerState = 'idle';
+			self.peer = null;
+			$(self).triggerHandler('hangup', data);
+		}
 	});
 
 	self._socket.emit('login', {user: userName});
@@ -58,4 +96,18 @@ ServerConnection.prototype = {
 		this.state = 'disconnected';
 		this.isConnected = false;
 	},
+	
+	call: function(callee) {
+		if (!this.isConnected)
+			throw new Error("tried to call, but is not connected");
+		this.playerState = 'calling';
+		this.peer = callee;
+		this._socket.emit('call', {recipient: callee});
+	},
+	
+	hangup: function() {
+		this.playerState = 'idle';
+		this.peer = null;
+		this._socket.emit('hangup');
+	}
 };
--- spec/server-connection.js
+++ spec/server-connection.js
@@ -1,9 +1,10 @@
 describe('ServerConnection', function() {
-	var uniqueUserName = null;
+	function getUniqueUserName() {
+		return 'user' + Math.floor(Math.random() * 10000);
+	}
 	
 	beforeEach(function() {
 		ServerConnection.WEBSOCKET_URL = 'http://localhost:8888';	
-		uniqueUserName = 'user' + Math.floor(Math.random() * 10000);
 	});
 	
 	it('should be connecting after creation', function() {
@@ -15,11 +16,12 @@ describe('ServerConnection', function() {
 	});
 	
 	it('should connect', function() {
-		var connection = new ServerConnection(uniqueUserName);
+		var connection = new ServerConnection(getUniqueUserName());
 		
 		waitsFor(function() {
 			return connection.isConnected;
 		}, 'isConnected to be true');
+		
 		runs(function() {
 			expect(connection.state).toEqual('connected');
 			
@@ -28,7 +30,9 @@ describe('ServerConnection', function() {
 	});
 	
 	it('should not connect if user name is not available', function() {
-		var connection1 = new ServerConnection(uniqueUserName);
+		var userName = getUniqueUserName();
+		
+		var connection1 = new ServerConnection(userName);
 		var connection2 = null;
 		
 		waitsFor(function() {
@@ -36,7 +40,7 @@ describe('ServerConnection', function() {
 		}, 'first connection to be established');
 		
 		runs(function() {
-			connection2 = new ServerConnection(uniqueUserName);
+			connection2 = new ServerConnection(userName);
 		});
 		
 		waitsFor(function() {
@@ -50,27 +54,34 @@ describe('ServerConnection', function() {
 		});
 	});
 	
-	it('should receive contact list', function() {
-		var connection = new ServerConnection(uniqueUserName);
-		var contactsReceived = false;
+	it('should receive player list', function() {
+		var connection = new ServerConnection(getUniqueUserName());
 		
-		$(connection).on('contacts', function(e, contacts) {
-			contactsReceived = true;
+		var playersListReceived = false;
+		$(connection).on('players', function(e, contacts) {
+			playersListReceived = true;
 		});
 		
 		waitsFor(function() {
-			return contactsReceived;
-		}, 'contacts event to be fired');
+			return playersListReceived;
+		}, 'players event to be fired');
 		
 		runs(function() {
-			expect(connection.contacts.length).not.toBeLessThan(1);
+			var players = connection.players;
+			expect(players.length).not.toBeLessThan(1);
+			for (var i = 0; i < players.length; i++) {
+				expect(typeof(players[i].name)).toBe('string');
+				expect(typeof(players[i].state)).toBe('string');
+			}
 
 			connection.close();
 		});
 	});
 	
 	it('should free user name on disconnect', function() {
-		var connection = new ServerConnection(uniqueUserName);
+		var userName = getUniqueUserName();
+		
+		var connection = new ServerConnection(userName);
 		
 		waitsFor(function() {
 			return connection.isConnected;
@@ -78,7 +89,7 @@ describe('ServerConnection', function() {
 		
 		runs(function() {
 			connection.close();
-			connection = new ServerConnection(uniqueUserName);
+			connection = new ServerConnection(userName);
 		});
 		
 		waitsFor(function() {
@@ -88,6 +99,296 @@ describe('ServerConnection', function() {
 			expect(connection.isConnected).toBe(true);
 
 			connection.close();
+		});
+	});
+	
+	it('should allow to call a player', function() {
+		var connection1 = new ServerConnection(getUniqueUserName());
+		var connection2 = new ServerConnection(getUniqueUserName());
+
+		var user2Called = false;
+		$(connection2).on('call', function(e, data) {
+			if (data.sender == connection1.userName)
+				user2Called = true;
+		});
+		
+		waitsFor(function() {
+			return connection1.isConnected && connection2.isConnected;
+		}, 'both connections to be established');
+		
+		runs(function() {
+			connection1.call(connection2.userName);
+		});
+		
+		waitsFor(function() {
+			return user2Called;
+		}, 'user2 to be called');
+
+		var user2ReceivedPlayerListUpdate = false;
+		$(connection2).on('players', function(e, data) {
+			var players = connection2.players;
+			for (var i = 0; i < players.length; i++) {
+				if (players[i].name == connection1.userName && players[i].state == 'calling') {
+					user2ReceivedPlayerListUpdate = true;
+					return;
+				}
+			}
+		});
+		
+		waitsFor(function() {
+			return user2ReceivedPlayerListUpdate;
+		}, 'player1 to be seen as "calling" for player2');
+		
+		runs(function() {
+			connection1.close();
+			connection2.close();
+		});
+	});
+	
+	it('should allow to reject a call', function() {
+		var connection1 = new ServerConnection(getUniqueUserName());
+		var connection2 = new ServerConnection(getUniqueUserName());
+		
+		waitsFor(function() {
+			return connection1.isConnected && connection2.isConnected;
+		}, 'both connections to be established');
+
+		var user2Called = false;
+		$(connection2).on('call', function(e, call) {
+			if (call.sender == connection1.userName) {
+				user2Called = true;
+				call.reject();
+			}
+		});
+
+		var callRejected = false;
+		$(connection1).on('reject', function(e, data) {
+			if (data.sender == connection2.userName) {
+				callRejected = true;
+			}
+		});
+		
+		runs(function() {
+			connection1.call(connection2.userName);
+		});
+		
+		waitsFor(function() {
+			return user2Called;
+		}, 'user2 to be called');
+		
+		waitsFor(function() {
+			return callRejected;
+		}, 'call to be rejected');
+		
+		runs(function() {
+			connection1.close();
+			connection2.close();
+		});
+	});
+	
+	it('should allow to accept a call', function() {
+		var connection1 = new ServerConnection(getUniqueUserName());
+		var connection2 = new ServerConnection(getUniqueUserName());
+		
+		waitsFor(function() {
+			return connection1.isConnected && connection2.isConnected;
+		}, 'both connections to be established');
+
+		var user2Called = false;
+		$(connection2).on('call', function(e, call) {
+			if (call.sender == connection1.userName) {
+				user2Called = true;
+				call.accept();
+			}
+		});
+
+		var callAccepted = false;
+		$(connection1).on('accept', function(e, data) {
+			if (data.sender == connection2.userName) {
+				callAccepted = true;
+			}
+		});
+
+		var user1ReceivedPlayerListUpdate = false;
+		$(connection1).on('players', function(e, data) {
+			var players = connection1.players;
+			for (var i = 0; i < players.length; i++) {
+				if (players[i].name == connection2.userName && players[i].state == 'busy') {
+					user1ReceivedPlayerListUpdate = true;
+					return;
+				}
+			}
+		});
+		
+		runs(function() {
+			connection1.call(connection2.userName);
+		});
+		
+		waitsFor(function() {
+			return user2Called;
+		}, 'user2 to be called');
+		
+		waitsFor(function() {
+			return callAccepted;
+		}, 'call to be accepted');
+		
+		waitsFor(function() {
+			return user1ReceivedPlayerListUpdate;
+		}, 'user state to be set to "busy"');
+		
+		runs(function() {
+			connection1.close();
+			connection2.close();
+		});
+	});
+	
+	it('should allow to hang up', function() {
+		var connection1 = new ServerConnection(getUniqueUserName());
+		var connection2 = new ServerConnection(getUniqueUserName());
+		
+		waitsFor(function() {
+			return connection1.isConnected && connection2.isConnected;
+		}, 'both connections to be established');
+
+		var user2ReceivedCall = false;
+		$(connection2).on('call', function(e, call) {
+			if (call.sender == connection1.userName) {
+				user2ReceivedCall = true;
+				call.accept();
+			}
+		});
+
+		var callAccepted = false;
+		$(connection1).on('accept', function(e, data) {
+			if (data.sender == connection2.userName) {
+				callAccepted = true;
+				connection1.hangup();
+			}
+		});
+
+		var user2ReceivedHangUp = false;
+		$(connection2).on('hangup', function(e, data) {
+			user2ReceivedHangUp = true;
+		});
+
+		var playerListUpdatePart1 = false;
+		var playerListUpdatePart2 = false;
+		$(connection1).on('players', function(e, data) {
+			var players = connection1.players;
+			for (var i = 0; i < players.length; i++) {
+				if (players[i].name == connection2.userName && players[i].state == 'idle') {
+					playerListUpdatePart2 = true;
+				}
+			}
+		});
+		$(connection2).on('players', function(e, data) {
+			var players = connection2.players;
+			for (var i = 0; i < players.length; i++) {
+				if (players[i].name == connection1.userName && players[i].state == 'idle') {
+					playerListUpdatePart1 = true;
+				}
+			}
+		});
+		
+		runs(function() {
+			connection1.call(connection2.userName);
+		});
+		
+		waitsFor(function() {
+			return user2ReceivedCall;
+		}, 'user2 to be called');
+		
+		waitsFor(function() {
+			return callAccepted;
+		}, 'call to be accepted');
+		
+		waitsFor(function() {
+			return user2ReceivedHangUp;
+		}, 'call to be hung up');
+		
+		waitsFor(function() {
+			return playerListUpdatePart1 && playerListUpdatePart2;
+		}, 'both user states to be set to "idle"');
+		
+		runs(function() {
+			connection1.close();
+			connection2.close();
+		});
+	});
+	
+	it('should hang up call between A and B when A calls C', function() {
+		var connection1 = new ServerConnection(getUniqueUserName());
+		var connection2 = new ServerConnection(getUniqueUserName());
+		var connection3 = new ServerConnection(getUniqueUserName());
+		
+		waitsFor(function() {
+			return connection1.isConnected && connection2.isConnected && connection3.isConnected;
+		}, 'all three connections to be established');
+
+		var user2ReceivedCall = false;
+		$(connection2).on('call', function(e, call) {
+			if (call.sender == connection1.userName) {
+				user2ReceivedCall = true;
+				call.accept();
+			}
+		});
+
+		var callAccepted = false;
+		$(connection1).on('accept', function(e, data) {
+			if (data.sender == connection2.userName) {
+				callAccepted = true;
+				connection1.call(connection3.userName);
+			}
+		});
+
+		var user2RecivedHangUp = false;
+		$(connection2).on('hangup', function(e, data) {
+			user2RecivedHangUp = true;
+		});
+
+		var playerListUpdatePart1 = false;
+		var playerListUpdatePart2 = false;
+		$(connection1).on('players', function(e, data) {
+			var players = connection1.players;
+			for (var i = 0; i < players.length; i++) {
+				if (players[i].name == connection2.userName && players[i].state == 'idle') {
+					playerListUpdatePart2 = true;
+				} 
+			}
+		});
+		$(connection2).on('players', function(e, data) {
+			var players = connection2.players;
+			for (var i = 0; i < players.length; i++) {
+				if (players[i].name == connection1.userName && players[i].state == 'calling') {
+					playerListUpdatePart1 = true;
+				}
+			}
+		});
+		
+		runs(function() {
+			connection1.call(connection2.userName);
+		});
+		
+		waitsFor(function() {
+			return user2ReceivedCall;
+		}, 'user2 to be called');
+		
+		waitsFor(function() {
+			return callAccepted;
+		}, 'call to be accepted');
+		
+		waitsFor(function() {
+			return user2RecivedHangUp;
+		}, 'call to be hung up');
+		
+		waitsFor(function() {
+			return playerListUpdatePart1 && playerListUpdatePart2;
+		}, 'user 1 to set to "calling" and user 2 to be set to "idle"');
+		
+		runs(function() {
+			connection1.close();
+			connection2.close();
+			connection3.close();
 		});
 	});
 });
